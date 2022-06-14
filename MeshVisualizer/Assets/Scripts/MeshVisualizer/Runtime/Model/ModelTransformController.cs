30b76e185117729474c2f910c544be873e03c8e7
Tristan Lucas <tlucas.u@gmail.com>
2022-06-14 16:25:36 -0700

    Added Constraints to ModelTransformController and updated method names

--- MeshVisualizer/Assets/Scripts/MeshVisualizer/Runtime/Model/ModelTransformController.cs
+++ MeshVisualizer/Assets/Scripts/MeshVisualizer/Runtime/Model/ModelTransformController.cs
@@ -1,20 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-public class ModelTransformController : MonoBehaviour
-{
-    public void SetPosition(Vector3 newPosition) {
+public class ModelTransformController : MonoBehaviour {
+ 
+    [Header("Local Position Constraints")]
+    [InspectorName("X Axis")]
+    public MinMax xPositionConstraint = new MinMax(-1, 1);
+    [InspectorName("Y Axis")] 
+    public MinMax yPositionConstraint = new MinMax(-1, 1);
+    [InspectorName("Z Axis")] 
+    public MinMax zPositionConstraint = new MinMax(-1, 1);
+    
+    
+    public void SetLocalPositionWithConstraints(Vector3 position) {
         //Local position is used to allow for the model anchor to be nested in another
         //game object and be localed else where in the world 
-        transform.localPosition = newPosition;
+        transform.localPosition = ClampPosition(position);
+    }
+
+    public void TranslateWithConstraints(Vector3 translation) {
+        transform.Translate(translation);
+        transform.localPosition = ClampPosition(transform.localPosition);
+    }
+
+    private Vector3 ClampPosition(Vector3 position) {
+        return new Vector3() {
+            x = Mathf.Clamp(position.x, xPositionConstraint.min, xPositionConstraint.max),
+            y = Mathf.Clamp(position.y, yPositionConstraint.min, yPositionConstraint.max),
+            z = Mathf.Clamp(position.z, zPositionConstraint.min, zPositionConstraint.max),
+        };
     }
     
-    public void SetRotation(Vector3 newRotation) {
+    public void SetLocalRotation(Vector3 newRotation) {
         transform.localRotation = Quaternion.Euler(newRotation);
     }
-    
-    public void SetScale(float scale) {
+
+    public void Rotate(Vector3 axis, float angle) {
+        transform.Rotate(axis, angle);
+    }
+
+    public void SetLocalScale(float scale) {
         transform.localScale = Vector3.one * scale;
+    }
+    
+    [Serializable]
+    public struct MinMax {
+        public float max;
+        public float min;
+
+        public MinMax(float min, float max) {
+            this.min = min;
+            this.max = max;
+        }
     }
 }
