dd9e50ac3bb5a10688117f69246162ab76aafb63
Tristan Lucas <tlucas.u@gmail.com>
2022-06-14 16:24:50 -0700

    Updated ModelAssetController to support multiple textures (normal, ao, metallic, etc)

--- MeshVisualizer/Assets/Scripts/MeshVisualizer/Runtime/Model/ModelAssetController.cs
+++ MeshVisualizer/Assets/Scripts/MeshVisualizer/Runtime/Model/ModelAssetController.cs
@@ -1,70 +1,124 @@
+using System;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
-using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceLocations;
 
 namespace MeshVisualizer {
     public class ModelAssetController : MonoBehaviour {
+
+        [SerializeField]
+        [AssetReferenceUILabelRestriction("Model")]
+        private AssetReferenceGameObject startingModel;
+        
+        [SerializeField]
+        [AssetReferenceUILabelRestriction("Material")]
+        private AssetReference startingMaterial;
+        
+        [SerializeField]
+        [AssetReferenceUILabelRestriction("Texture")]
+        private AssetReference startingTexture;
+        
         private string currentModelAssetKey { get; set; }
-        private string currentMaterialAssetKey { get; set; }
-        private string currentTextureAssetKey { get; set; }
-        
-        private GameObject currentModel { get; set; }
-        
+        public GameObject currentModel { get; private set; }
         private MeshRenderer modelMeshRenderer { get; set; }
-        private Texture currentTexture { get; set; }
-        private Material currentMaterial { get; set; }
+        
+        
+        private string currentMaterialAssetKey { get; set; }
+        public Material currentMaterial { get;  private set; }
+        
+        
+        private string currentTextureAssetKey { get; set; }
+        public Material currentTextureMaterial { get; private set; }
+
+        private bool destroyed { get; set; } = false;
+
+        private void Awake() {
+            if (startingModel != null && startingModel.RuntimeKeyIsValid()) {
+                Addressables.LoadResourceLocationsAsync(startingModel).Completed += handle => {
+                    IResourceLocation resourceLocation = handle.Result.First();
+                    SwitchModel(resourceLocation.PrimaryKey);
+                    Addressables.Release(handle);
+                };
+            }
+
+            if (startingMaterial != null && startingMaterial.RuntimeKeyIsValid()) {
+                Addressables.LoadResourceLocationsAsync(startingMaterial).Completed += handle => {
+                    IResourceLocation resourceLocation = handle.Result.First();
+                    SwitchMaterial(resourceLocation.PrimaryKey);
+                    Addressables.Release(handle);
+                };
+            }
+
+            if (startingTexture != null && startingTexture.RuntimeKeyIsValid()) {
+                Addressables.LoadResourceLocationsAsync(startingTexture).Completed += handle => {
+                    IResourceLocation resourceLocation = handle.Result.First();
+                    SwitchTexture(resourceLocation.PrimaryKey);
+                    Addressables.Release(handle);
+                };
+            }
+        }
+
+        private void OnDestroy() {
+            destroyed = true;
+        }
 
 
         public void SwitchModel(string assetKey) {
             if (currentModelAssetKey == assetKey)
                 return;
             
-            if (currentModel != null) {
-                Addressables.ReleaseInstance(currentModel);
-                modelMeshRenderer = null;
-            }
-
             currentModelAssetKey = assetKey;
-            Addressables.InstantiateAsync(assetKey, this.transform, false).Completed += OnModelLoadComplete;
-        }
+            Addressables.InstantiateAsync(assetKey).Completed += handle => {
+                if (destroyed) {
+                    Addressables.Release(handle);
+                    return;
+                }
 
-        private void OnModelLoadComplete(AsyncOperationHandle<GameObject> handle) {
-            SetModel(handle.Result);
+                GameObject instance = handle.Result;
+                instance.transform.SetParent(this.transform, false);
+                SetModel(instance);
+            };
         }
 
         public void SwitchMaterial(string assetKey) {
             if (currentMaterialAssetKey == assetKey)
                 return;
-            
-            if (currentMaterial != null) {
-                Addressables.Release(currentMaterial);
-            }
 
             currentMaterialAssetKey = assetKey;
-            Addressables.LoadAssetAsync<Material>(assetKey).Completed += OnMaterialLoadComplete;
-        }
-
-        private void OnMaterialLoadComplete(AsyncOperationHandle<Material> handle) {
-            SetMaterial(handle.Result);
+            Addressables.LoadAssetAsync<Material>(assetKey).Completed += handle => {
+                if (destroyed) {
+                    Addressables.Release(handle);
+                    return;
+                }
+                
+                Material instance = Instantiate(handle.Result);
+                SetMaterial(instance);
+                Addressables.ReleaseInstance(handle);
+            };
         }
 
         public void SwitchTexture(string assetKey) {
             if (currentTextureAssetKey == assetKey)
                 return;
-            
-            if (currentTexture != null) {
-                Addressables.Release(currentTexture);
-            }
 
             currentTextureAssetKey = assetKey;
-            Addressables.LoadAssetAsync<Texture>(assetKey).Completed += OnTextureLoadComplete;
+            Addressables.LoadAssetAsync<Material>(assetKey).Completed += handle => {
+                if (destroyed) {
+                    Addressables.Release(handle);
+                    return;
+                }
+                
+                SetTexture(handle.Result);
+            };
         }
-
-        private void OnTextureLoadComplete(AsyncOperationHandle<Texture> handle) {
-            SetTexture(handle.Result);
-        }
-
+        
         private void SetModel(GameObject model) {
+            if (currentModel != null) {
+                Addressables.ReleaseInstance(currentModel);
+                modelMeshRenderer = null;
+            }
+            
             currentModel = model;
 
             if (model == null)
@@ -75,21 +129,49 @@ namespace MeshVisualizer {
         }
 
         private void SetMaterial(Material material) {
-            currentMaterial = material;
+            if (material != currentMaterial && currentMaterial != null) {
+                DestroyImmediate(currentMaterial);
+            }
 
+            currentMaterial = material;
+            
             if (modelMeshRenderer == null)
                 return;
 
             modelMeshRenderer.material = material;
-            SetTexture(currentTexture);
+            
+            SetTexture(currentTextureMaterial);
         }
 
-        private void SetTexture(Texture texture) {
-            currentTexture = texture;
-            if (modelMeshRenderer == null)
+        private void SetTexture(Material textureMaterial) {
+            if (textureMaterial != currentTextureMaterial && currentTextureMaterial != null) {
+                Addressables.Release(currentTextureMaterial);
+            }
+            
+            ClearTextures();
+
+            currentTextureMaterial = textureMaterial;
+
+            if (currentMaterial == null || textureMaterial == null)
                 return;
 
-            modelMeshRenderer.material.mainTexture = texture;
+            string[] textureProperties = textureMaterial.GetTexturePropertyNames();
+            foreach (var textureProperty in textureProperties) {
+                if (currentMaterial.HasTexture(textureProperty)) {
+                    Texture texture = textureMaterial.GetTexture(textureProperty);
+                    currentMaterial.SetTexture(textureProperty, texture);
+                }
+            }
+        }
+
+        private void ClearTextures() {
+            if (currentMaterial == null)
+                return;
+            
+            string[] textureProperties = currentMaterial.GetTexturePropertyNames();
+            foreach (var textureProperty in textureProperties) {
+                currentMaterial.SetTexture(textureProperty, null);
+            }
         }
     }
 }
